462fd324d05f4e824785104414ded337024f7d43
Jack Radcliffe <jack.radcliffe@postgrad.manchester.ac.uk>
2018-02-27 16:37:26 +0100

    changes

--- pbcor_CASA.py
+++ pbcor_CASA.py
@@ -1,24 +1,36 @@
 from recipes import makepb
 import sys, os
 
-msfile = str(sys.argv[3])
-imagetemplate = str(sys.argv[4])
+try:
+	msfile = str(sys.argv[sys.argv.index('pbcor_CASA.py')+1])
+	imagetemplate = str(sys.argv[sys.argv.index('pbcor_CASA.py')+2])
+except IndexError:
+	print('Usage casa -c pbcor_CASA.py <msfile> <image>')
 
-print sys.argv
-print imagetemplate[-4:]
-
+print '\npbcor_CASA generating images for your VLA needs and wants, huzzah\n'
 if imagetemplate[-1] == '/':
 	imagetemplate = imagetemplate[:-1]
 
 if imagetemplate[-4:]== 'fits' or imagetemplate[-4:]=='FITS':
+	print 'FITS file detected - converting to ms\n'
 	importfits(fitsimage=imagetemplate, imagename=imagetemplate[:-4]+'image')
 	imagetemplate = imagetemplate[:-4]+'image'
+
+	print 'Using makepb recipe to generate pb\n'
 	makepb.makePB(vis=msfile,field='0',imtemplate=imagetemplate,outimage=msfile[:-2]+'pb',pblimit=0.1)
+
+	print 'Primary beam correcting image, new pbcor image = %s\n' % imagetemplate[:-6]+'_pbcor.image'
 	impbcor(imagename=imagetemplate, pbimage=msfile[:-2]+'pb', outfile=imagetemplate[:-6]+'_pbcor.image')
+
+	print 'Exporting to pb and pbcor image to fits\n'
 	exportfits(imagename=imagetemplate[:-6]+'_pbcor.image', fitsimage=imagetemplate[:-6]+'_pbcor.fits')
 	exportfits(imagename=msfile[:-2]+'pb', fitsimage=msfile[:-2]+'pb.fits')
 else:
+	print 'No fits file detected, assuming image name is CASA image... sorry\n'
 	makepb.makePB(vis=msfile,field='0',imtemplate=imagetemplate,outimage=msfile[:-2]+'pb',pblimit=0.1)
-	impbcor(imagename=imagetemplate, pbimage=msfile[:-2]+'pb', outfile=imagetemplate[:-6]+'_pbcor.image')
-	exportfits(imagename=imagetemplate[:-6]+'_pbcor.image', fitsimage=imagetemplate[:-6]+'_pbcor.fits')
+
+	print 'Primary beam correcting image, new pbcor image = %s\n' % imagetemplate+'.pbcor'
+	impbcor(imagename=imagetemplate, pbimage=msfile[:-2]+'pb', outfile=imagetemplate+'_pbcor')
+	print 'Exporting to pb and pbcor image to fits\n'
+	exportfits(imagename=imagetemplate+'.pbcor', fitsimage=imagetemplate+'.pbcor.fits')
 	exportfits(imagename=msfile[:-2]+'pb', fitsimage=msfile[:-2]+'pb.fits')
